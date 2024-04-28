13242c2cc6ef1c0ceabfbf82a6dcba8d12fa8a4f
Eden <160818732+edenevans111@users.noreply.github.com>
2024-04-28 10:23:03 -0600

    Add files via upload

--- freetime_activities_pt2.py
+++ freetime_activities_pt2.py
@@ -1,22 +1,58 @@
-import sys
-#for this file I need to have it read the file made in pt1
-#then I need it to randomly come up with one of the activities
-#I would also like to have it save a note for each activity
-#about where I left off the last time I did it
-def read_file(input):
-    filename = input
-    input_file = open(filename, 'r')
-    activities = []
-    for line in input_file:
-        activity = input_file.readline()
-        activities.append(line)
-    return activities
+class Activities:
+    all_activities = []
 
-def get_activity(lst):
-    pass
+    def __init__(self, activity):
+        self.activity = activity
+        self.notes = []
+        Activities.all_activities.append(self.activity)
+
+    def new_note(self, note): #need to be sure there is a note before printing
+        self.notes.append(note)
+
+    def print_all_notes(self):
+        for note in self.notes:
+            return f'{self.activity} : {note}\n'
+
+    def print_all_activities(self):
+        for self.activity in Activities.all_activities:
+            print(self.activity)
+
+    def delete_activity(self, activity):
+        Activities.all_activities.remove(f'{activity}')
+        del self
+        print(f'{activity} has been deleted')
+
+    def print_activty_note(self, activity):
+        print(f'{activity} : {self.notes[-1]}')
+    def __str__(self):
+        if self.notes[-1]:
+            return f'{self.activity} : {self.notes[-1]}'
+        else:
+            return f'{self.activity}'
+    def __repr__(self):
+        return f'{self.activity}, {self.notes}'
+
+
 
 def main():
-    print(read_file(sys.argv[1]))
+    print('view activities\n'
+               'add activity\n'
+               'delete activity\n'
+                "view activity's notes\n"
+                'find activity\n')
+    answer = input('Which would you like to do?\n')
+    if answer == 'view activities':
+        for activity_obj in Activities.all_activities:
+            print(activity_obj)
+    if answer == 'add activity': #this is where a new activity is added with a note
+        new_activity = input('What is the new activity?')
+        new_activity = Activities(new_activity)
+        new_note_obj = input(f'What is the note for {new_activity}')
+        Activities.new_note(new_note_obj)
+    else:
+        print('You have not written programming for that yet')
+
+
 
 if __name__ == '__main__':
     main()
