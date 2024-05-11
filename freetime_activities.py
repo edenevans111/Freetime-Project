603bd095e6a2fb0998a45c78f9b7289f022e40ce
Eden <160818732+edenevans111@users.noreply.github.com>
2024-05-10 23:07:00 -0600

    Add files via upload

--- freetime_activities.py
+++ freetime_activities.py
@@ -1,25 +1,38 @@
-import freetime_activities_pt2
+import freetime_activities_pt2 as activities
 import random
 
-def make_list():
-    activities = []
+def make_list(): # This whole thing works for making the list
+    lst = []
     while True:
-        activity = input('Add activity: ')
-        if activity == '':
+        item = input('Add activity: ')
+        if item == '':
             break
         else:
-            activities.append(activity)
-    return activities
+            lst.append(item)
+    return lst
+
+def write_lines(lst): # I need to test this to make sure that it works
+    output_filename = 'list_of_activities.csv'
+    with open(output_filename, 'w') as file:
+        for item in lst:
+            file.write(f'{item}\n')
+    return output_filename
+
+def make_classes(lst):
+    for item in lst:
+        instance = activities(f'{item}') # Need to figure out the problem here
+
 # I think we also need a function that will turn each item in the list to classes
 
 def main():
-    activities = make_list()
-    answer = input('What would you like to do?:'
-                   'Generate activity'
-                   'Show list of activities')
+    free_times = make_list()
+    write_lines(free_times)
+    answer = input('What would you like to do?:\n'
+                   'Generate activity\n'
+                   'Show list of activities\n')
     if answer == 'Generate activity':
-        random.choice(activities)
-        print()
+        random.choice(free_times)
+        print(random.choice(free_times)) # this should print the activity class and last note
     # we want this one to generate an activity from the list
     # then we want it to print that activity and its note
 
